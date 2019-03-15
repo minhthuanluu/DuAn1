f22be08546d5f7825ae13c5934c9306eacf5b50c
JohnLuu <minhthuan.luu@gmail.com>
2019-03-15 19:56:36 +0700

    Xong database_helloAct_drawerlayout

---
+++ app/src/main/java/com/example/johnluu/duan1/model/Chuong.java
@@ -0,0 +1,24 @@
+package com.example.johnluu.duan1.model;
+
+public class Chuong {
+    public int _idchuong;
+    public int _idsach;
+    public String duongdan;
+    public String audio;
+
+    public Chuong() {
+    }
+
+    public Chuong(int _idsach, String duongdan, String audio) {
+        this._idsach = _idsach;
+        this.duongdan = duongdan;
+        this.audio = audio;
+    }
+
+    public Chuong(int _idchuong, int _idsach, String duongdan, String audio) {
+        this._idchuong = _idchuong;
+        this._idsach = _idsach;
+        this.duongdan = duongdan;
+        this.audio = audio;
+    }
+}
